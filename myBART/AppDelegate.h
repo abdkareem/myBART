db2f299a0fd43b2b8cd2df7b89a7007917edf36b
root <abd.khaja@gmail.com>
2017-01-23 10:34:43 -0800

    Initial Commit

---
+++ myBART/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myBART
+//
+//  Created by Taiyaba Sultana on 1/23/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myBART/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBART
+//
+//  Created by Taiyaba Sultana on 1/23/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
